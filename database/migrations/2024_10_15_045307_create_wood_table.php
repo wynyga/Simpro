7c02be172ac74a8a32be26d81014e31871b42491
Can <wayanyoga32@gmail.com>
2024-10-15 11:59:50 +0700

    Migration

---
+++ database/migrations/2024_10_15_045307_create_wood_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('wood', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode')->virtualAs("CONCAT('WOD70-', LPAD(id, 2, '0'))");
+            $table->string('jenis_tenaga_peralatan');
+            $table->string('uty');
+            $table->string('satuan');
+            $table->decimal('harga_satuan', 18, 2);
+            $table->integer('stock_bahan');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('wood');
+    }
+};
---
+++ database/migrations/2024_10_15_045527_create_paint_glass_wallpaper_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('paint_glass_wallpaper', function (Blueprint $table) {
+            $table->id();
+            $table->string('kode')->virtualAs("CONCAT('PGW100-', LPAD(id, 2, '0'))");
+            $table->string('jenis_tenaga_peralatan');
+            $table->string('uty');
+            $table->string('satuan');
+            $table->decimal('harga_satuan', 18, 2);
+            $table->integer('stock_bahan');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('paint_glass_wallpaper');
+    }
+};
