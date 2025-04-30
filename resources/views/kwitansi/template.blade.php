39fd4d77e03961ca2066790482af16801cb6e788
Can <wayanyoga32@gmail.com>
2025-05-01 03:46:34 +0800

    Kwitansi template

--- resources/views/kwitansi/template.blade.php
+++ resources/views/kwitansi/template.blade.php
@@ -1,97 +1,92 @@
 <!DOCTYPE html>
 <html lang="id">
-<head>
-    <meta charset="UTF-8">
-    <title>Kwitansi - {{ $kwitansi->no_doc }}</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            font-size: 12px;
-            margin: 20px;
-        }
-        .header {
-            text-align: center;
-            margin-bottom: 20px;
-        }
-        .section {
-            margin-bottom: 10px;
-        }
-        table {
-            width: 100%;
-        }
-        .signature-table {
-            width: 100%;
-            margin-top: 40px;
-            text-align: center;
-        }
-        .signature-table td {
-            padding: 0 10px;
-        }
-        .bold {
-            font-weight: bold;
-        }
-        .text-right {
-            text-align: right;
-        }
-        .text-left {
-            text-align: left;
-        }
-        .small {
-            font-size: 10px;
-        }
-    </style>
-</head>
-<body>
+    <head>
+        <meta charset="UTF-8">
+        <title>Kwitansi - {{ $kwitansi->no_doc }}</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                font-size: 12px;
+                margin: 40px;
+            }
 
-    <div class="header">
-        <h2>KWITANSI</h2>
-        <p>No. Doc: {{ $kwitansi->no_doc }}</p>
-    </div>
+            .header {
+                text-align: center;
+                margin-bottom: 20px;
+            }
 
-    <div class="section">
-        <table>
+            .section {
+                margin-bottom: 10px;
+            }
+
+            table {
+                width: 100%;
+            }
+
+            .signature-table {
+                width: 100%;
+                margin-top: 40px;
+                text-align: center;
+                table-layout: fixed;
+            }
+
+            .signature-table td {
+                width: 33%;
+                word-wrap: break-word;
+                padding: 0 10px;
+            }
+
+            .small {
+                font-size: 10px;
+            }
+        </style>
+    </head>
+    <body>
+
+        <div class="header">
+            <h2>KWITANSI</h2>
+            <p>No. Doc: {{ $kwitansi->no_doc }}</p>
+            <p>Tanggal: {{ \Carbon\Carbon::parse($kwitansi->tanggal)->format('d M Y') }}</p>
+        </div>
+
+        <div class="section">
+            <table>
+                <tr>
+                    <td width="30%">Sudah terima dari</td>
+                    <td>: {{ $kwitansi->dari }}</td>
+                </tr>
+                <tr>
+                    <td>Uang Sejumlah</td>
+                    <td>: Rp {{ number_format($kwitansi->jumlah, 2, ',', '.') }}</td>
+                </tr>
+                <tr>
+                    <td>Untuk Pembayaran</td>
+                    <td>: {{ $kwitansi->untuk_pembayaran }}</td>
+                </tr>
+                <tr>
+                    <td>Jenis Penerimaan</td>
+                    <td>: {{ $kwitansi->jenis_penerimaan }}</td>
+                </tr>
+            </table>
+        </div>
+
+        <table class="signature-table">
             <tr>
-                <td width="30%">Sudah terima dari</td>
-                <td>: {{ $kwitansi->dari }}</td>
+                <td >Mengetahui,</td>
+                <td >Dibuat Oleh,</td>
+                <td >Disetor Oleh,</td>
+            </tr>
+            <tr style="height: 60px;">
+                <td style="padding-top: 40px;">( ......................... )</td>
+                <td style="padding-top: 40px;">( ......................... )</td>
+                <td style="padding-top: 40px;">( ......................... )</td>
             </tr>
             <tr>
-                <td>Uang Sejumlah</td>
-                <td>: Rp {{ number_format($kwitansi->jumlah, 2, ',', '.') }}</td>
-            </tr>
-            <tr>
-                <td>Untuk Pembayaran</td>
-                <td>: {{ $kwitansi->untuk_pembayaran }}</td>
-            </tr>
-            <tr>
-                <td>Jenis Penerimaan</td>
-                <td>: {{ $kwitansi->jenis_penerimaan }}</td>
+                <td class="small" style="margin-top: 10px;">Direktur</td>
+                <td class="small" style="margin-top: 10px;">Kasir</td>
+                <td class="small" style="margin-top: 10px;">Penyetor</td>
             </tr>
         </table>
-    </div>
 
-    <div class="section">
-        <p class="small text-left">
-            <strong>Catatan:</strong> Kwitansi ini dianggap sah setelah diterima dan dibukukan.
-        </p>
-    </div>
-
-    <table class="signature-table">
-        <tr>
-            <td>Mengetahui,</td>
-            <td>Dibuat Oleh,</td>
-            <td>Disetor Oleh,</td>
-        </tr>
-        <tr>
-            <td height="60px">(........................................)</td>
-            <td>(........................................)</td>
-            <td>(........................................)</td>
-        </tr>
-        <tr>
-            <td class="small">Direktur</td>
-            <td class="small">Kasir</td>
-            <td class="small">Penyetor</td>
-        </tr>
-    </table>
-
-</body>
+    </body>
 </html>
